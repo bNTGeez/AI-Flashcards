1445b8d0755b320ccec9988457341d98c6ee0cfa
Benjamin <tangbenjamin123l@gmail.com>
2024-08-19 00:27:13 -0700

    l

--- app/firebase.js
+++ app/firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtTM0y6hLmYDVyzWrxGQ-M5FnQvsS3FOQ",
-  authDomain: "aiflashcards-c957d.firebaseapp.com",
-  projectId: "aiflashcards-c957d",
-  storageBucket: "aiflashcards-c957d.appspot.com",
-  messagingSenderId: "148396886441",
-  appId: "1:148396886441:web:80a2c11c80b4ecd7cddcb8",
-  measurementId: "G-VQJ4043K4E",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
